14c3c59ca61a80f96e2c109b65cac707e5c1fb1e
Ramazan BIYIK <ramazanbiyik@Ramazans-MacBook-Pro.local>
2023-01-02 14:29:34 +0300

    test

---
+++ .github/workflows/check-variables.sh
@@ -0,0 +1,32 @@
+echo "ECR REPOSITORY: $1";
+echo "Previous Revision Number: $2";
+new_revision_number=$2
+is_exist_imagetag=true
+is_exist_definition=true
+# Check if Image Exist or not
+while "$is_exist_imagetag"; do 
+	IMAGE_TAG=$(aws ecs describe-task-definition --task-definition "$1":"$new_revision_number" --query "taskDefinition.containerDefinitions[0].image" --output text |cut -d: -f2)
+	if (aws ecr describe-images --repository-name=$1 --image-ids=imageTag="$IMAGE_TAG"&> /dev/null); then
+		echo "IMAGE FOUND"
+		echo "CHECKING TASK DEFINITION"
+		# Check if definition exist or not
+		while "$is_exist_definition"; do 
+			if (aws ecs describe-task-definition --task-definition "$1:$new_revision_number" --output text &> /dev/null); then
+				echo "TASK DEFINITION FOUND"
+				is_exist_definition=false
+				is_exist_imagetag=false
+			else
+				echo 'TASK DEFINITION IS NOT FOUND'
+				new_revision_number=$(( $new_revision_number - 1 ))
+			fi
+		done
+
+		is_exist_imagetag=false
+	else
+		echo 'Image is Not Found on ECR'
+		new_revision_number=$(( $new_revision_number - 1 ))
+	fi
+done
+
+echo "New revision number: $new_revision_number"
+echo "PREVIOUS_REVISION_NUMBER"=$new_revision_number >> $GITHUB_ENV
